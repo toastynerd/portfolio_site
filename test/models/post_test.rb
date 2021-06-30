bbb663c603978e52610843cc3890edaa7125dcf2
toastynerd <tylermorgan86@gmail.com>
2021-06-30 01:12:09 -0700

    add and test the post model

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @post_params = {
+      title: "a test title",
+      body: "here is a non empty body"
+    }
+  end
+
+  test "should require a title" do
+    post = Post.new
+    assert_not post.save
+  end
+
+  test "should require a body" do
+    post = Post.new(title: "here is a test title")
+    assert_not post.save
+  end
+
+  test "should save with valid data" do
+    post =Post.new(@post_params)
+    assert post.save
+  end
 end
