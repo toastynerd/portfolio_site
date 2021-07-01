38c4a5ac379a5aa35a040f97e797f08f1c58151e
toastynerd <tylermorgan86@gmail.com>
2021-07-01 12:32:53 -0700

    post controller and tests

--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -1,6 +1,11 @@
 require "test_helper"
 
 class PostsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @post_params = {title: "first test post", body: "here is the body"}
+    @post_edit_params = {title: "second test post title", body: "another different body"}
+  end
+
   test "should have an index" do
     get posts_path
     assert_response :success
@@ -13,28 +18,69 @@ class PostsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "it should have an individual action for a single post" do
+    get post_path posts(:test_post)
+
+    test_post = @controller.instance_variable_get('@post')
+    assert_not_nil test_post, "no post found in controller action"
+    assert_equal "a test post", test_post.title, "post does not have right title"
+    assert_response :success
+  end
+
   test "it should create a new post with valid data" do
-    #TODO write this test
-    flunk "test has not been written yet"
+    assert_difference("Post.count") do
+      post posts_path, params: {post: @post_params}
+    end
+
+    test_post = @controller.instance_variable_get('@post')
+    assert_not_nil test_post
+    assert_equal @post_params[:title], test_post.title
+    assert_equal @post_params[:body], test_post.body
+
+    assert_redirected_to post_path(Post.last)
   end
 
   test "it should be able to get an edit page for an existing post" do
-    #TODO: write this test
-    flunk "test has not been written yet"
+    get edit_post_path posts(:test_post)
+
+    test_post = @controller.instance_variable_get('@post')
+    assert_not_nil test_post
+    assert_equal test_post, posts(:test_post)
+
+    assert_response :success
   end
 
   test "it should be able to update with a PATCH request" do
-    #TODO: write this test
-    flunk "test has not been written yet"
+    assert_no_difference("Post.count") do
+      patch post_path posts(:test_post), params: {post: @post_edit_params}
+    end
+
+    test_post = @controller.instance_variable_get('@post')
+    assert_not_nil test_post
+    assert_equal @post_edit_params[:title], test_post.title
+    assert_equal @post_edit_params[:body], test_post.body
+
+    assert_redirected_to post_path posts(:test_post)
   end
 
   test "it should be able to update with a PUT request" do
-    #TODO: write this test
-    flunk "test has not been written yet"
+    assert_no_difference("Post.count") do
+      put post_path posts(:test_post), params: {post: @post_edit_params}
+    end
+
+    test_post = @controller.instance_variable_get('@post')
+    assert_not_nil test_post
+    assert_equal @post_edit_params[:title], test_post.title
+    assert_equal @post_edit_params[:body], test_post.body
+
+    assert_redirected_to post_path posts(:test_post)
   end
 
   test "it should be able to a delete a post" do
-    #TODO: write this test
-    flunk "test has not been written yet"
+    assert_difference("Post.count", -1) do
+      delete post_path posts(:test_post)
+    end
+
+    assert_redirected_to posts_path
   end
 end
