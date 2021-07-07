b9fd74447703a36ad2e0bdccdfc03866c190f0e2
toastynerd <tylermorgan86@gmail.com>
2021-07-07 00:54:51 -0700

    some additional validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   has_secure_password
 
   validate :valid_password_confirmation, on: :create
-  validates :email, :username, presence: true
+  validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: 'Invalid email' }
+  validates :username, presence: true, uniqueness: true
 
   private
   def valid_password_confirmation
