169265460712464d3da8149001152729f9e8bab6
toastynerd <tylermorgan86@gmail.com>
2021-07-09 11:55:18 -0700

    some UI cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts
-  root "static#home"
+  root 'static#home'
+  get 'about', to: 'static#about'
 
   # authentication routes
   get 'sign_up', to: 'registrations#new'
