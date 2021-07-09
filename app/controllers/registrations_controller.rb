dcb09aaa5be7ce3af346172ff68094b45ed8fcf1
toastynerd <tylermorgan86@gmail.com>
2021-07-08 20:59:51 -0700

    add views for sign up and sign in

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,6 +5,7 @@ class RegistrationsController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    puts user_params
     if @user.save
       session[:user_id] = @user.id
       redirect_to posts_path
