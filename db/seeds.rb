1706756267f557ef8e2d884730121d4b185608f0
Tyler Morgan <toasty@Tylers-Mac-Pro.local>
2021-07-21 23:57:26 -0700

    add seed file for contact links

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Link.create({
+  icon_url: "icons/github_small.png",
+  linkable_type: "User",
+  linkable_id: "1",
+  url: "https://github.com/toastynerd",
+  description: "github"
+})
+
+Link.create({
+  icon_url: "icons/li_small.png",
+  linkable_type: "User",
+  linkable_id: "1",
+  url: "https://www.linkedin.com/in/tyler-morgan-b3a82853/",
+  description: "linkedin"
+})
